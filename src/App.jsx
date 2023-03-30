080d3ec1653963bf63e9dbf5f2cb9e535c31bfde
ATTIPARK <qkrqjawls0922@gmail.com>
2023-03-30 18:16:42 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">포트폴리오</h1>
+      <h2 className="text-3xl font-bold underline">준비</h2>
+      <h3 className="text-3xl font-bold underline">🥤</h3>
+    </div>
+  );
+}
+
+export default App;
